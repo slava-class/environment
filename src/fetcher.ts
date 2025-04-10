b463af613f7bea4ea22b8b6822a2ba0b656000f6
Yuri <yuri@tbqh.net>
2025-04-10 17:02:16 +1000

    valibot

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,26 +1,44 @@
+import * as v from "valibot";
+
 export const fetchers = {
-    getExample: async <T>(url: string): Promise<T> => {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+	getExample: async <T>(url: string): Promise<T> => {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
 
-      return await response.json() as T;
-    },
-  
-    postExample: async <T>(url: string, data: unknown): Promise<T> => {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
+		return (await response.json()) as T;
+	},
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+	postExample: async <T>(url: string, data: unknown): Promise<T> => {
+		const response = await fetch(url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		});
 
-      return await response.json() as T;
-    },
-  };
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+
+		return (await response.json()) as T;
+	},
+};
+
+const exampleSchema = v.strictObject({
+	foo: v.literal("bar"),
+});
+
+export const typedFetcher = {
+	getExample: async () => {
+		const response = await fetchers.getExample("");
+		const parseResult = v.safeParse(exampleSchema, response);
+		if (parseResult.success) {
+			return parseResult.output;
+		}
+		console.log(parseResult.issues);
+		throw new Error("Parsing failed");
+	},
+};
